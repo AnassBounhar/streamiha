ef49990a7d1394f0bb1eeb394763f595d15aa59b
Anass BOUNHAR <a.bounhar@gmail.com>
2026-04-29 22:39:34 +0100

    fix: sandbox player iframe to block top-level redirects

--- pages/player.php
+++ pages/player.php
@@ -98,6 +98,7 @@ if ($postedUrl !== '') {
         id="player-frame"
         src="<?php echo htmlspecialchars($embedUrl, ENT_QUOTES, 'UTF-8'); ?>"
         referrerpolicy="no-referrer"
+        sandbox="allow-scripts allow-same-origin allow-forms allow-presentation allow-popups"
         allow="autoplay; fullscreen; picture-in-picture"
       ></iframe>
     </div>
